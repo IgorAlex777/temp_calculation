c402e7f59136db910d6f2e7df119d1a8e0a86631
IgorAlexCmex <ia777i888@gmail.com>
2024-11-27 08:30:45 +0300

    добавил databinding

--- app/src/main/java/com/cmex/fragmentsandadaptivescreen/domain/Result.kt
+++ app/src/main/java/com/cmex/fragmentsandadaptivescreen/domain/Result.kt
@@ -8,4 +8,9 @@ data class Result(
     val flagResult:Boolean,
     val sumNumbersMin: Int,
     val sumNumbersMax:Int
-):Parcelable
+):Parcelable{
+    val minSumString:String
+        get() = "$sumNumbersMin-min Sum"
+    val maxSumString:String
+        get() = "$sumNumbersMax-max Sum"
+}
